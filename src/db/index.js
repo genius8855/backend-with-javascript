498c1f09a649d8bacfdb74a11e529fd8d744d9dd
Sahil <aradhyy076@gmail.com>
2024-09-19 15:11:18 +0530

    database connection updated

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect("mongodb+srv://sahil8855:sahil8855@cluster0.1okpb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}`)
         console.log(`\n MongoDB connected !! DB HOSt: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("connection error", error);
